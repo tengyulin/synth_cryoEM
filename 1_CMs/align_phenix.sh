aa93001d90f20de8c43eb7af0ecdf3d27da02931
tengyu lin <danlinbarista0302@gmail.com>
2022-11-02 10:44:16 +0800

    align

--- 1_CMs/align_phenix.sh
+++ 1_CMs/align_phenix.sh
@@ -1,9 +1,9 @@
 # script to re-center all PDBs based on common center-of-mass coordinates
 
-for i in /home/danlin/synth/1_CMs/CM2/*.pdb
+for i in /home/danlin/synth_cryoEM/1_CMs/CM2/*.pdb
 do
     out=`basename $i`
-    outFile=/home/danlin/synth/1_CMs/align_CMs/$out
+    outFile=/home/danlin/synth_cryoEM/1_CMs/align_CMs/$out
     echo "$outFile"
     phenix.pdbtools "${i}" output.file_name=$outFile sites.translate="-105.95 -104.70 -104.87" #replace with CoM of your pdb
 done
